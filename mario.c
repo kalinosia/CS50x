4a42e18b5d10ca0d2a8d7b1e73dec8deb591f7a9
kalinosia <kalinosia@users.noreply.github.com>
2020-08-05 14:11:38 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -10,29 +10,21 @@ int main(void)
     {
         n = get_int("Height: ");
     }
-    /*
-    int q=n-1;
-    char znaki[n];
-    char odwrocone[n];
-    printf("TU: %s  %s\n\n", znaki, odwrocone);
-    for(int j=0; j<n; j++){
-        znaki[j]='_';
-        odwrocone[j]='_';
-    }
-    */
-    int h=2*n+2;
-    char linia[h];
-    for(int j=0; j<h; j++){
-        linia[j]=' ';
-    }
-    linia[h]='\0';
     int z=1;
-    for(int i=0; i<n; i++){
-        linia[n-z]='#';
-        linia[n+z+1]='#';
-        printf("%s\n", linia);
+     for (int i = 0; i < n; i++){
+        for(int k=0; k<n-z; k++){
+            printf(" ");
+         }
+        for (int j = 0; j < z; j++){
+          printf("#");
+        }
+        printf("  ");
+        for (int j = 0; j < z; j++){
+          printf("#");
+        }
+        printf("\n");
         z++;
-    }
+        }
     
    
 }
