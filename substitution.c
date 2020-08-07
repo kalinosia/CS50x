5938035953b00283459cc1a7f347cec95408f31a
kalinosia <kalinosia@users.noreply.github.com>
2020-08-07 12:06:41 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -95,7 +95,7 @@ int Array[len];
     int index[strlen(plaintext)];//indexs in IN
     int ciphertext[strlen(plaintext)]; //OUT
     const int alfabetAll[52]={65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-        97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,11};
+        97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122};
     //array TEXT to NUMBER
     unsigned int arrayPlaintext[strlen(plaintext)];
     for (int i=0; i < strlen(plaintext); i++){
