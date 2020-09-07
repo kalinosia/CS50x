7eede1cc7a045673c8a64cfbc384a0db00ed4d2e
kalinosia <kalinosia@users.noreply.github.com>
2020-09-07 12:11:26 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -34,12 +34,15 @@ bool check(const char *word)
     
     //we need this?? probably not
     char *name = malloc(sizeof(char[strlen(word)])); //len(word) = [LENGTH + 1] ? 
-    strcpy(name, word);
+    //strcpy(name, word);
     
     for (int a = 0; a < strlen(word); a++){ 
         if (word[a]>=95 && (int) word[a]<=90){
             name[a]=word[a]+26;
+        }else{
+            name[a]=word[a];
         }
+        
     }
     
     int address=0;
